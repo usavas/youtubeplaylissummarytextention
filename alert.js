3d7912c9e544bd4e9540dfe27b767d11468f7631
usavas <savasusame@gmail.com>
2020-05-13 14:12:25 +0300

    add styling bug fixes

--- alert.js
+++ alert.js
@@ -1,20 +1,16 @@
 var title = document.querySelector(
   "div#container div#header-contents div#header-description h3"
 ).innerText;
-console.log(title);
-
 
 var videos = document.querySelectorAll(
   "div#secondary div#container div#items.playlist-items #playlist-items"
 );
 
-
 var summaryList = [];
-summaryList.push('Title: ' + title);
+summaryList.push({ title: "Title: ", time: title });
 var totalVideoTimeInSec = 0;
 
 videos.forEach((element) => {
-    
   var videoTime = element.querySelector(
     "#container #thumbnail-container ytd-thumbnail-overlay-time-status-renderer span"
   ).innerText;
@@ -23,19 +19,18 @@ videos.forEach((element) => {
     "div#container div#meta h4 span#video-title"
   ).innerText;
 
-  summaryList.push("\n" + videoTitle + ' - ' + videoTime);
+  summaryList.push({ title: videoTitle, time: videoTime });
 
-
-  var times = videoTime.split(':');
+  var times = videoTime.split(":");
 
   totalVideoTimeInSec += parseInt(times[times.length - 1]);
   totalVideoTimeInSec += parseInt(times[times.length - 2]) * 60;
-  if(times.length > 2){
+  if (times.length > 2) {
     totalVideoTimeInSec += parseInt(times[0]) * 60 * 60;
   }
 });
 
-function getTotalTimeInString(totalTimeInSeconds, playSpeed){
+function getTotalTimeInString(totalTimeInSeconds, playSpeed) {
   let totalTime = totalTimeInSeconds / playSpeed;
   totalTime = Math.floor(totalTime);
   var hours, minutes, seconds;
@@ -43,20 +38,84 @@ function getTotalTimeInString(totalTimeInSeconds, playSpeed){
   totalTime %= 3600;
   minutes = Math.floor(totalTime / 60);
   seconds = totalTime % 60;
-  let totalTimeInString = hours + ':' + minutes + ':' + seconds;
+  let totalTimeInString = hours + ":" + minutes + ":" + seconds;
   return totalTimeInString;
 }
 
-console.log(summaryList.join('\n'));
+summaryList.splice(5, 0, {
+  title: " Total Time: ",
+  time: getTotalTimeInString(totalVideoTimeInSec, 1),
+});
 
-summaryList[0] += (' Total Time: ' + getTotalTimeInString(totalVideoTimeInSec, 1));
-
-summaryList[1] = 'Total time in 1.25 speed: ' + getTotalTimeInString(totalVideoTimeInSec, 1.25);
-summaryList[2] = 'Total time in 1.5 speed ' + getTotalTimeInString(totalVideoTimeInSec, 1.5);
-summaryList[3] = 'Total time in 1.75 speed: ' + getTotalTimeInString(totalVideoTimeInSec, 1.75);
-summaryList[4] = 'Total time in 2 speed: ' + getTotalTimeInString(totalVideoTimeInSec, 2);
-
+summaryList.splice(4, 0, {
+  title: "Total time in 1.25 speed: ",
+  time: getTotalTimeInString(totalVideoTimeInSec, 1.25),
+});
+summaryList.splice(3, 0, {
+  title: "Total time in 1.5 speed ",
+  time: getTotalTimeInString(totalVideoTimeInSec, 1.5),
+});
+summaryList.splice(2, 0, {
+  title: "Total time in 1.75 speed: ",
+  time: getTotalTimeInString(totalVideoTimeInSec, 1.75),
+});
+summaryList.splice(1, 0, {
+  title: "Total time in 2 speed: ",
+  time: getTotalTimeInString(totalVideoTimeInSec, 2),
+});
 
 var sumWindow = window.open("", "sumWindow", "width= 600,height:400");
-sumWindow.document.write(summaryList.join('<br>'));
 
+var tableDom = sumWindow.document.createElement("table");
+tableDom.cellSpacing = '10px';
+tableDom.style.fontSize = '20px';
+
+var tHead = tableDom.createTHead();
+var tHeaderRow = tHead.insertRow();
+
+var colTitle = sumWindow.document.createTextNode('Title');
+var hCell1 = tHeaderRow.insertCell(0);
+hCell1.appendChild(colTitle);
+tHeaderRow.appendChild(hCell1);
+
+var colValue = sumWindow.document.createTextNode('Value');
+var hCell2 = tHeaderRow.insertCell(1);
+hCell2.appendChild(colValue);
+tHeaderRow.appendChild(hCell2);
+
+tHead.appendChild(tHeaderRow);
+tableDom.appendChild(tHead);
+
+function addRowToTable(title, time2) {
+  let tRow = tBody.insertRow();
+
+  let cell1 = tRow.insertCell(0);
+  let cellText1 = sumWindow.document.createTextNode(title);
+  cell1.appendChild(cellText1);
+
+  let cell2 = tRow.insertCell(1);
+  let cellText2 = sumWindow.document.createTextNode(time2);
+  cell2.appendChild(cellText2);
+
+  tRow.appendChild(cell1);
+  tRow.appendChild(cell2);
+  tBody.appendChild(tRow);
+}
+
+function addScrollMargin() {
+  var scrollMargin = sumWindow.document.createElement("div");
+  scrollMargin.style.height = "500px";
+  sumWindow.document.body.appendChild(scrollMargin);
+}
+
+var tBody = tableDom.createTBody();
+
+summaryList.forEach((element) => {
+  addRowToTable(element.title, element.time);
+});
+tableDom.appendChild(tBody);
+
+sumWindow.document.body.appendChild(tableDom);
+addScrollMargin();
+
+console.log(tableDom);
